9f050ee859775d715ec833d3399c568c9679e88e
stas- <stas-petrushenko@mail.ru>
2020-12-29 13:50:01 +0300

    Урок 7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {connect} from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  addTrack() {
+    this.props.onAddTrack(this.trackInput.value);
+    this.trackInput.value = '';
+  }
+
+  cleanTracks() {
+      this.props.onCleanTracks();
+      this.trackInput.value = '';
+  }
+
+    findTrack() {
+        console.log('findTrack', this.searchInput.value);
+        this.props.onFindTrack(this.searchInput.value)
+    }
+
+  render() {
+    return (
+        <React.Fragment>
+            <div>
+                <input type="text" ref={(input) => { this.trackInput = input; }} />
+                <button onClick={this.addTrack.bind(this)}>Add track</button>
+                <ul>
+                    {this.props.tracks.map((track, index) =>
+                        <li key={index}>{track.name}</li>
+                    )}
+                </ul>
+                <button onClick={this.cleanTracks.bind(this)}>Clean</button>
+            </div>
+            <div>
+                <input type="text" ref={(input) => { this.searchInput = input }} />
+                <button onClick={this.findTrack.bind(this)}>Find track</button>
+            </div>
+        </React.Fragment>
+    )
+  }
 }
 
-export default App;
+export default connect (
+    state => ({
+        tracks: state.tracks.filter(track => track.name.includes(state.filterTracks))
+    }),
+    dispatch => ({
+      onAddTrack: (name) => {
+          const payload = {
+              id: Date.now().toString(),
+              name
+          }
+        dispatch({ type: 'ADD_TRACK', payload })
+      },
+      onCleanTracks:() => {
+          dispatch({ type: 'CLEAN_TRACKS' })
+      },
+        onFindTrack: (name) => {
+            dispatch({ type: 'FIND_TRACK', payload: name })
+        },
+
+    })
+)(App)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reducer from './reducers';
+
+
+
+// Создаем store
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// импортируем store
+// import {createStore} from "redux";
+//
+// const addTrackBtn = document.querySelectorAll('.addTrack')[0];
+// const list = document.querySelectorAll('.list')[0];
+// const trackInput = document.querySelectorAll('.trackInput')[0];
+//
+// // Получение значение store
+// store.subscribe(() => {
+//     // console.log(store.getState())
+//     list.innerHTML = '';
+//     store.getState().forEach(track => {
+//         const li = document.createElement('li');
+//         li.textContent = track;
+//         list.appendChild(li);
+//     })
+//     trackInput.value = '';
+// })
+//
+//
+//
+// addTrackBtn.addEventListener('click', () => {
+//     const trackName = trackInput.value;
+//     store.dispatch({ type: 'ADD_TRACK', payload: trackName });
+// });
